4395d2f45cfa3a08e6e45ec3206547a592e6eeca
svc-student <svc-student@student.directory.jccc>
2018-03-06 16:48:50 -0600

    Initial commit to master brancH

---
+++ Assets/TurretControl.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TurretControl : MonoBehaviour {
+
+
+    public float minBarrelElevation = 15.0f;
+    public float maxBarrelElevation = 75.0f;
+
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
